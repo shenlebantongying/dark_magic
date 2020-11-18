b904a758e3d36c8fc968bbcc9dbbfccde6cf9e53
slb <jiang.zhengchao@outlook.com>
2020-11-18 08:37:41 -0500

    scheme

---
+++ ocaml/README.md
@@ -0,0 +1,10 @@
++  The OCaml system https://ocaml.org/releases/4.11/htmlman/index.html>
+
++ Real World OCaml <https://dev.realworldocaml.org/>
+
+
+
+# Other related links
+
+* [Xavier Leroy](https://xavierleroy.org/)
+
--- scheme/hello.scm
+++ scheme/hello.scm
@@ -1,1 +1,62 @@
-(display 1)
+;; Tested on Chez Scheme
+
+
+;; TODO call/cc
+
+;; =================================
+;; Syntactic Extension
+
+;; let is nothing special
+;; (let ((var expr) ...) body1 body2 ...)
+;; is equivalent to
+;; ((lambda (var ...) body1 body2 ...)
+;;  expr ...)
+
+(define-syntax mylet
+  (syntax-rules ()
+    [(_ ((x e) ...) b1 b2 ...)
+     ((lambda (x ...) b1 b2 ...) e ...)]))
+
+(mylet ([x 1] [y 2]) (+ x y)) ;; => 3
+
+;; =================================
+;; Implementions of Higher-Order Functions
+
+(define [mymap f l]
+  (if (null? l)
+      '()
+      (cons [f (car l)] [mymap f (cdr l)])))
+
+(mymap [lambda (x) (+ x x)] '(1 2 3)) ;; => (2 4 6)
+
+;; =================================
+;; Continuation-passing style
+
+;; Example 2*3 + 1
+;; Normally
+(+ (* 2 3))
+
+;; CPS
+;; Define CPS version of the functions
+
+(define (cps+ x y c)
+  (c (+ x y)))
+
+(define (cps* x y c)
+  (c (* x y)))
+
+(define (cps_output x) x)
+
+(cps* 2 3 (lambda (x) (+ x 1))) ;; => 7
+
+;; example 1*2 + 3*4
+
+(cps* 1 2 [lambda (m12) (cps* 3 4 [lambda (m34) (+ m12 m34)])]) ;; =>14
+
+;; Execution:
+;; compute (+ 1 2)
+;; pass the result to m12 in [lambda (m12) (cps* 3 4 (lambda (m34) (+ m12 m34)))]
+;; compute (+ 3 4)
+;; pass the result to m34 in [lambda (m34) (+ m12 m34)]
+;; return the (+ m12 m34) => 14
+
